70eaac6c6f6954904ea60c9318be216219a999de
Jianlong Chen <jianlong99@gmail.com>
2017-10-12 08:29:56 +0800

    Extent FileWrapper

--- Notes-iOS/Document.swift
+++ Notes-iOS/Document.swift
@@ -7,6 +7,38 @@
 //
 
 import UIKit
+import MobileCoreServices
+
+extension FileWrapper {
+    var fileExtension : String? {
+        return self.preferredFilename?.components(separatedBy: ".").last
+    }
+    
+    func conformsToType(type: CFString) -> Bool {
+        guard let fileExtension = fileExtension else {
+            return false
+        }
+        
+        guard let fileType = UTTypeCreatePreferredIdentifierForTag(
+            kUTTagClassFilenameExtension, fileExtension as CFString, nil)?.takeRetainedValue() else {
+            return false
+        }
+        
+        return UTTypeConformsTo(fileType, type)
+    }
+    
+    func thumbnailImage() -> UIImage? {
+        if self.conformsToType(type: kUTTypeImage) {
+            guard let attachmentContent = self.regularFileContents else {
+                return nil
+            }
+            
+            return UIImage(data: attachmentContent)
+        }
+        
+        return nil
+    }
+}
 
 class Document: UIDocument {
     var text = NSAttributedString(string: "") {
@@ -42,5 +74,45 @@ class Document: UIDocument {
         self.text = try NSAttributedString(data: textFileData, options: [NSDocumentTypeDocumentAttribute: NSRTFTextDocumentType], documentAttributes: nil)
         
         self.documentFileWrapper = fileWrapper
+    }
+    
+    private var attachmentsDirectoryWrapper : FileWrapper? {
+        guard let fileWrappers = self.documentFileWrapper.fileWrappers else {
+            NSLog("Attempting to access document's contents, but none found!")
+            return nil
+        }
+    
+        var attachmentsDirectoryWrapper = fileWrappers[NoteDocumentFileNames.AttachmentDirectory.rawValue]
+    
+        if attachmentsDirectoryWrapper == nil {
+            attachmentsDirectoryWrapper = FileWrapper(directoryWithFileWrappers: [:])
+            attachmentsDirectoryWrapper?.preferredFilename = NoteDocumentFileNames.AttachmentDirectory.rawValue
+            
+            self.documentFileWrapper.addFileWrapper(attachmentsDirectoryWrapper!)
+            self.updateChangeCount(UIDocumentChangeKind.done)
+        }
+        
+        return attachmentsDirectoryWrapper
+    }
+    
+    dynamic var attachFiles : [FileWrapper]? {
+        guard let attachmentsFileWrappers = attachmentsDirectoryWrapper?.fileWrappers else {
+            NSLog("Can't access the attachments directory!")
+            return nil
+        }
+        
+        return Array(attachmentsFileWrappers.values)
+    }
+    
+    func addAttachmentAtURL(url: URL) throws -> FileWrapper {
+        guard attachmentsDirectoryWrapper != nil else {
+            throw err(code: .CannotAccessAttachments)
+        }
+        
+        let newAttachment = try FileWrapper(url: url, options: FileWrapper.ReadingOptions.immediate)
+        
+        attachmentsDirectoryWrapper?.addFileWrapper(newAttachment)
+        self.updateChangeCount(UIDocumentChangeKind.done)
+        return newAttachment
     }
 }
